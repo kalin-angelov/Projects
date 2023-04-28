a6814fe209913706a7ccd04c3192a9c73af7f793
kalin-angelov <kalinangelov@abv.bg>
2023-04-28 15:02:34 +0300

    Update App.js

--- Little Game/game/src/App.js
+++ Little Game/game/src/App.js
@@ -1,17 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
-
 import { Test } from './components/Test/Test';
-import { CardTrue } from './components/CardTrue/CardTrue';
-import { CardFalse } from './components/CardFalse/CardFalse';
 
 function App() {
   return (
     <div >
-      <Routes>
-        <Route path='/' element={<Test />} />
-        <Route path='/true' element={<CardTrue />} />
-        <Route path='/false' element={<CardFalse />} />
-      </Routes>
+      <Test />
     </div>
   );
 }
