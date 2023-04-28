c383e1bbc498e5d814790acc04a6368e13739e52
kalin-angelov <kalinangelov@abv.bg>
2023-04-28 15:02:22 +0300

    Update Test.js

--- Little Game/game/src/components/Test/Test.js
+++ Little Game/game/src/components/Test/Test.js
@@ -1,11 +1,36 @@
-import { Link } from 'react-router-dom';
+
+import { useState } from 'react';
+
+import { CardTrue } from '../CardTrue/CardTrue';
+import { CardFalse } from '../CardFalse/CardFalse';
 
 export const Test = () => {
+    const [isTrueCard, setIsTrueCard] = useState(false);
+    const [isFalseCard, setIsFalseCard] = useState(false);
+    const onClickIsTrue = () => {
+        setIsTrueCard(true);
+
+        setTimeout(() => {
+            setIsTrueCard(false);
+        },4000);
+    }
+
+    const onClickIsFalse = () => {
+        setIsFalseCard(true);
+
+        setTimeout(() => {
+            setIsFalseCard(false);
+        },4000);
+    }
+
     return(
         <div>
             <h1>Hello Test</h1>
-            <Link to={'/true'}>True</Link>
-            <Link to={'/false'}>False</Link>
+            <button onClick={onClickIsTrue}>True</button>
+            <button onClick={onClickIsFalse}>False</button>
+
+            {isTrueCard && <CardTrue />}
+            {isFalseCard && <CardFalse />}
         </div>
     )
 }
