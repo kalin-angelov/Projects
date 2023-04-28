5ce413ae7f422e9be85bf0d9a448061353e9bc83
kalin-angelov <kalinangelov@abv.bg>
2023-04-28 15:01:47 +0300

    Update CardTrue.js

--- Little Game/game/src/components/CardTrue/CardTrue.js
+++ Little Game/game/src/components/CardTrue/CardTrue.js
@@ -4,10 +4,7 @@ export const CardTrue = () => {
     return (
         <div className={styles.card}>
             <div className={styles.cardFront}>
-                <img src="/images/card-front-true.png" alt="card-front-true" />
-            </div>
-            <div className={styles.cardBack}>
-                <img src="/images/card-back.png" alt="card-Back" />
+                <img src="/images/card-front-true.png" alt="card-front-true" className={styles.cardImg} />
             </div>
         </div>
     );
