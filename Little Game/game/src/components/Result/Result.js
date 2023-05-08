eab0e0b9f103d1b48573aca55b20cad91e2236ac
kalin-angelov <kalinangelov@abv.bg>
2023-05-08 11:54:51 +0300

    Create Result.js

---
+++ Little Game/game/src/components/Result/Result.js
@@ -0,0 +1,28 @@
+import styles from "./Result.module.css";
+
+import { useLocation, useNavigate } from "react-router-dom";
+
+import data from '../../data/miniData.json';
+
+export const Result = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const userResult = location.state;
+    console.log(userResult);
+
+    const onPlayAgain = () => {
+        navigate('/');
+    }
+
+    return (
+        <div className={styles.result}>
+            <div className={styles.headlineAndPoints}>
+                <h1>Funny Quiz</h1>
+                <h2>Welcome: {location.state.username}</h2>
+                <p>{location.state.points}/{data.length - 1}0</p>
+            </div>
+            <p>Result's</p>
+            <button onClick={onPlayAgain}>Play Again</button>
+        </div>
+    )
+};
