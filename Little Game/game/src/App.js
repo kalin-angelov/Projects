46af92839c95265181deefce4782c6907304fe75
kalin-angelov <kalinangelov@abv.bg>
2023-05-08 11:55:13 +0300

    Update App.js

--- Little Game/game/src/App.js
+++ Little Game/game/src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 
 import { Home } from "./components/Home/Home";
 import { Quiz } from "./components/Quiz/Quiz";
+import { Result } from "./components/Result/Result";
 import { Footer } from "./components/Footer/Footer";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
 
       <Footer />
