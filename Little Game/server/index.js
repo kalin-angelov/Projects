08d61fde15c75924a8cbec411587a4d314d618d9
kalin-angelov <kalinangelov@abv.bg>
2023-05-16 17:03:06 +0300

    Create index.js

---
+++ Little Game/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = require('./router');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+
+mongoose.connect('mongodb://127.0.0.1:27017/littleGame');
+
+app.listen(3030, () => console.log('Server is online and is running on port 3030...'));
