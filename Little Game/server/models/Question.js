cd86f99c1af64608e7357f654e7dd5341e81ff84
kalin-angelov <kalinangelov@abv.bg>
2023-05-16 17:04:13 +0300

    Create Question.js

---
+++ Little Game/server/models/Question.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        require:[true]
+    },
+    image: {
+        type: String,
+        required: [true, 'Image Is Required!'],
+        validate: {
+            validator: function(value) {
+                if (!value.startsWith('http') || !value.startsWith('https')) {
+                    throw new Error('Invalid Image URL!');
+                }
+            }
+        }
+    },
+    answerOne: {
+        type: String,
+        require: [true]
+    },
+    one: {
+        type: String,
+        require: [true],
+        enum: [true, false]
+    },
+    answerTwo: {
+        type: String,
+        require: [true]
+    },
+    two: {
+        type: String,
+        require: [true],
+        enum: [true, false]
+    },
+    answerThree: {
+        type: String,
+        require: [true]
+    },
+    three: {
+        type: String,
+        require: [true],
+        enum: [true, false]
+    },
+    answerFour:  {
+        type: String,
+        require: [true]
+    },
+    four: {
+        type: String,
+        require: [true],
+        enum: [true, false]
+    }
+});
+
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;
