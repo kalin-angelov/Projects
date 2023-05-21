8dddec2feb3b57ad37f508eb7c9860538720826d
kalin-angelov <kalinangelov@abv.bg>
2023-05-21 20:12:32 +0300

    Update index.js

--- Little Game/server/index.js
+++ Little Game/server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const router = require('./router');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
