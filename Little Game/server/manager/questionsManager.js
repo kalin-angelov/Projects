17fe179cb04c565c100d650c2ed631059974e149
kalin-angelov <kalinangelov@abv.bg>
2023-05-21 20:11:53 +0300

    Update questionsManager.js

--- Little Game/server/manager/questionsManager.js
+++ Little Game/server/manager/questionsManager.js
@@ -1,3 +1,4 @@
 const Question = require('../models/Question');
 
+exports.getAllQuestions = () => Question.find({});
 exports.createNewQuestion = ( question, image, answerOne, one, answerTwo, two, answerThree, three, answerFour, four ) => Question.create({ question, image, answerOne, one, answerTwo, two, answerThree, three, answerFour, four });
