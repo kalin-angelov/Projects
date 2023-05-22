da6b41372ecd23271bd7d41f4dd3886c433c9a41
kalin-angelov <kalinangelov@abv.bg>
2023-05-22 18:39:21 +0300

    Update router.js

--- Little Game/server/router.js
+++ Little Game/server/router.js
@@ -5,7 +5,7 @@ const questionController = require('./controllers/questionController');
 
 router.get('/littleGame/questions', questionController.getQuestions);
 
-router.post('/create/user', authController.postUser);
+router.post('/littleGame/create/user', authController.postUser);
 router.post('/create/question', questionController.postQuestion);
 
 module.exports = router;
