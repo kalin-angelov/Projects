cbfd678ba44e56954985ac939bf4685f21800025
kalin-angelov <kalinangelov@abv.bg>
2023-05-22 18:36:37 +0300

    Update questionsService.js

--- Little Game/game/src/service/questionsService.js
+++ Little Game/game/src/service/questionsService.js
@@ -1,6 +1,10 @@
 export const getAllQuestions = async () => {
-    const response = await fetch('http://localhost:3030/littleGame/questions');
-    const data = await response.json()
- 
-    return Object.values(data.questions);
+    try {
+        const response = await fetch('http://localhost:3030/littleGame/questions');
+        const data = await response.json();
+
+        return Object.values(data.questions);
+    } catch(error) {
+        return null;
+    }
 };
