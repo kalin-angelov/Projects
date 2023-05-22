47887f63f402f1c09b7ec69867e6d88f9888adee
kalin-angelov <kalinangelov@abv.bg>
2023-05-22 18:35:25 +0300

    Update Home.js

--- Little Game/game/src/components/Home/Home.js
+++ Little Game/game/src/components/Home/Home.js
@@ -3,18 +3,26 @@ import styles from './Home.module.css';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { getAllQuestions } from '../service/questionsService';
+import { getAllQuestions } from '../../service/questionsService';
+import { addNewUser } from '../../service/createUser';
 
 export const Home = () => {
     const navigate = useNavigate();
+
     const [username, setUsername] = useState('');
-    const [questions, setQuestions] = useState();
+    const [questions, setQuestions] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
 
     useEffect(() => {
         getAllQuestions()
             .then(questions => setQuestions(questions))
             .catch(error => console.log(error))
-    },[]);
+    }, []);
+
+    const errorHandler = (error) => {
+        setErrorMessage(error);
+        setUsername('');
+    };
 
     const onUsernameChange = (event) => {
         setUsername(event.target.value);
@@ -23,12 +31,24 @@ export const Home = () => {
     const onClickStart = async (event) => {
         event.preventDefault();
 
-        if (username !== '') {
+        if (questions === null) {
+            navigate('/404');
+            return;
+        };
+
+        try {
+            const user = await addNewUser(username);
+
             navigate('/quiz', {state: {
                 username: username, 
-                questions: questions
+                questions: questions,
+                user: user
             }});
-        };
+            
+        } catch (error) {
+            errorHandler(error);
+        }
+
     };
 
     const onClickESC = (event) => {
@@ -45,20 +65,37 @@ export const Home = () => {
         event.target.placeholder = "Enter you'r name here....";
     };
 
-    return(
+    return (
         <div className={styles.home}>
-            <form className={styles.form}>
-                <input 
-                    type="text" 
-                    placeholder="Enter you'r name here...." 
-                    name='username' 
+            <h1>Funny Quiz</h1>
+            <form>
+                {errorMessage === null ?
+                    <input
+                        className={styles.input}
+                        type="text"
+                        placeholder="Enter you'r name here...."
+                        name='username'
+                        autoComplete='off'
+                        onChange={onUsernameChange}
+                        value={username}
+                        onKeyDown={onClickESC}
+                        onFocus={onFocusIn}
+                        onBlur={onFocusOut}
+                    />
+                    :
+                    <input 
+                    className={styles.errorInput}
+                    type="text"
+                    placeholder={errorMessage}
+                    name='username'
                     autoComplete='off'
-                    onChange={onUsernameChange} 
+                    onChange={onUsernameChange}
                     value={username}
                     onKeyDown={onClickESC}
                     onFocus={onFocusIn}
                     onBlur={onFocusOut}
-                />
+                    />
+                }
                 <section >
                     <button className={styles.send} onClick={onClickStart}>Lets get started</button>
                 </section>
